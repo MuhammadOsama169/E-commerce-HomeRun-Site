1b4788bbbda70b10447029df33839d251fa9075a
MuhammadOsama169 <m.osama169@gmail.com>
2023-08-23 10:56:17 +0300

    style: add success animation on payment page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -2,6 +2,8 @@
 import { useSearchParams, useRouter } from 'next/navigation';
 import React, { useEffect } from 'react';
 import { LoadingSpinner } from '../components/Loading/LoadingSpinner';
+import Lottie from 'lottie-react';
+import animationSuccess from '../../public/assets/animation-success.json';
 
 const Success = () => {
   const searchParams = useSearchParams();
@@ -25,8 +27,11 @@ const Success = () => {
 
   return (
     <div className="flex items-center justify-center h-screen bg-gray-900">
-      <div className="max-w-md p-8 bg-gray-800 shadow-lg rounded-lg text-white">
-        <h1 className="text-3xl font-bold mb-4">Thank you for your payment!</h1>
+      <div className="max-w-md p-8 bg-gray-800 shadow-lg rounded-lg text-white ">
+        <Lottie className="h-[100px]" animationData={animationSuccess} />
+        <h1 className="text-3xl font-bold mb-4 text-center">
+          Thank you for your payment!
+        </h1>
         <p className="text-lg mb-6">
           Your transaction has been successfully processed. Please do not close
           the page. You will be redirected shortly!
