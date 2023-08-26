c82676243a3a89ce79c8faacc5004a594c66b9f0
MuhammadOsama169 <m.osama169@gmail.com>
2023-08-26 10:59:04 +0300

    bug fix: non-serializablevalue error redux-presist

--- app/store/index.js
+++ app/store/index.js
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import cartReducer from './state/cartSlice';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
@@ -13,5 +13,9 @@ export const store = configureStore({
   reducer: {
     cart: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 export const persistor = persistStore(store);
